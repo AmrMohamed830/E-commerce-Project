063c6ef0c2a73efff5fd3dd57cd5c268f82d68b5
AhmedMohamed320 <ahmedaltokey320@gmail.com>
2025-10-17 21:29:18 +0300

    add backend folder properly

---
+++ ecommerce-shopping/back-end/src/admin/vite.config.example.js
@@ -0,0 +1,11 @@
+import { mergeConfig } from 'vite';
+
+export default (config) => {
+  return mergeConfig(config, {
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  });
+};
